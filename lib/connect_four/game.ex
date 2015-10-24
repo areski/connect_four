e70ba5c3708384bf74101d5a742901ebe6104e52
Wendy Smoak <wsmoak@gmail.com>
2015-10-24 09:03:16 -0400

    initial code for a player's move

--- lib/connect_four/game.ex
+++ lib/connect_four/game.ex
@@ -25,4 +25,20 @@ defmodule ConnectFour.Game do
     ConnectFour.Board.print
   end
 
+  def move(player,column) do
+    case GenServer.call(@registered_name, {:move, player, column}) do
+      :ok -> "Successful move for #{player} player in column #{column}"
+    end
+  end
+
+  # Server Callbacks
+
+  def handle_call({:move, player, column}, _from, state) do
+    case ConnectFour.Board.place_token(player, column) do
+      :move_accepted ->
+        newstate = Map.put(state, :last_moved, player)
+        {:reply, :ok, newstate}
+    end
+  end
+
 end
